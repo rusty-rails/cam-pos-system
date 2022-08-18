a862d6aa52f8c5e4cd46652a79a5166421eeb774
Christian M <chriamue@gmail.com>
2022-08-18 10:32:38 +0200

    :sparkles: implemented predict to image in mobilenet

--- src/ssd/mobilenet.rs
+++ src/ssd/mobilenet.rs
@@ -1,9 +1,10 @@
-use super::predictable::Predictable;
-use crate::bbox::BBox;
+use super::predictable::{detect_objects, windows, Predictable};
 use crate::detection::Detection;
-use image::RgbImage;
-use image::{DynamicImage, GenericImageView};
+use image::DynamicImage;
+use image::{RgbImage, Rgba};
+use imageproc::drawing::{draw_cross_mut, draw_hollow_rect_mut, draw_text_mut};
 use imageproc::rect::Rect;
+use rusttype::{Font, Scale};
 use std::io::Cursor;
 use tract_onnx::prelude::*;
 use tract_onnx::tract_core::dims;
@@ -73,24 +74,6 @@ impl MobileNet {
         let index = best.unwrap().1;
         Ok(Some(index))
     }
-
-    pub fn scale(width: u32, height: u32, abox: &BBox) -> Rect {
-        let r = abox.scale_to_rect(width as i32, height as i32);
-        Rect::at(r.0, r.1).of_size(r.2, r.3)
-    }
-
-    pub fn detect_objects(
-        &self,
-        image: &Box<DynamicImage>,
-    ) -> Result<Vec<Detection>, Box<dyn std::error::Error>> {
-        let mut detections: Vec<Detection> = Vec::new();
-        Ok(detections)
-    }
-
-    pub fn run(&self, image: &DynamicImage) -> DynamicImage {
-        let mut img_copy = image.to_rgba8();
-        DynamicImage::ImageRgba8(img_copy)
-    }
 }
 
 impl Default for MobileNet {
@@ -142,32 +125,54 @@ impl Predictable for MobileNet {
         result
     }
 
-    fn predict_image(&self, window: RgbImage) -> Vec<u32> {
-        let resized = image::imageops::resize(
-            &window,
-            self.input_width as u32,
-            self.input_height as u32,
-            ::image::imageops::FilterType::Triangle,
-        );
-        let tensor: Tensor = tract_ndarray::Array4::from_shape_fn(
-            (1, self.input_height, self.input_width, 3),
-            |(_, y, x, c)| resized[(x as _, y as _)][c] as f32 / 255.0,
-        )
-        .into();
-
-        let result = self.model.run(tvec!(tensor)).unwrap();
-        let best = result[0]
-            .to_array_view::<f32>()
-            .unwrap()
-            .iter()
-            .cloned()
-            .zip(0..)
-            .max_by(|a, b| a.0.partial_cmp(&b.0).unwrap());
-        let index = best.unwrap().1;
-        vec![index]
+    fn predict_image(&self, image: RgbImage) -> Vec<u32> {
+        let (_cols, _rows, windows) = windows(&image, self.input_width as u32);
+        let windows = windows.iter().map(|window| window.to_image()).collect();
+        self.predict(windows)
     }
+
     fn predict_to_image(&self, image: RgbImage) -> DynamicImage {
-        todo!();
+        let window_size = self.input_width as u32;
+        let cols = 2 * (image.width() / window_size);
+        let rows = 2 * (image.height() / window_size);
+        let predictions = self.predict_image(image.clone());
+        let detections = detect_objects(cols, rows, predictions, window_size);
+
+        let mut img_copy = DynamicImage::ImageRgb8(image).to_rgba8();
+        for detection in detections.iter() {
+            let color = Rgba([125u8, 255u8, 0u8, 0u8]);
+            draw_cross_mut(
+                &mut img_copy,
+                Rgba([255u8, 0u8, 0u8, 0u8]),
+                detection.bbox.x as i32,
+                detection.bbox.y as i32,
+            );
+            draw_hollow_rect_mut(
+                &mut img_copy,
+                Rect::at(
+                    (detection.bbox.x as u32) as i32,
+                    (detection.bbox.y as u32) as i32,
+                )
+                .of_size(window_size, window_size),
+                color,
+            );
+
+            let font_data = include_bytes!("../../res/Arial.ttf");
+            let font = Font::try_from_bytes(font_data as &[u8]).unwrap();
+
+            const FONT_SCALE: f32 = 10.0;
+
+            draw_text_mut(
+                &mut img_copy,
+                Rgba([125u8, 255u8, 0u8, 0u8]),
+                detection.bbox.x as u32,
+                detection.bbox.y as u32,
+                Scale::uniform(FONT_SCALE),
+                &font,
+                &format!("{}", detection.class),
+            );
+        }
+        DynamicImage::ImageRgba8(img_copy)
     }
 }
 
@@ -180,8 +185,7 @@ mod tests {
     #[test]
     fn default() {
         let model = MobileNet::default();
-        let img = Box::new(image::open("res/webcam06.jpg").unwrap());
-        let detections = model.detect_objects(&img);
+        assert_eq!(model.input_height, 96);
     }
 
     #[test]
@@ -252,5 +256,17 @@ mod tests {
             model.detect_class(&DynamicImage::ImageRgb8(loco5)).unwrap(),
             Some(5)
         );
+    }
+
+    #[test]
+    fn test_predict_to_image() {
+        let webcam1 = open("res/webcam01.jpg").unwrap().to_rgb8();
+
+        let model = MobileNet::default();
+
+        model
+            .predict_to_image(webcam1)
+            .save("out/test_predict_to_image_webcam1.png")
+            .unwrap();
     }
 }
