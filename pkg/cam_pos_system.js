ac2bea28bc89e218b3999e81f2ca27fdfb9da463
chriamue <chriamue@users.noreply.github.com>
2022-09-04 07:05:56 +0000

    deploy: 183baf0ced379b1368071a8231ebe4997fb4612d

--- pkg/cam_pos_system.js
+++ pkg/cam_pos_system.js
@@ -747,11 +747,11 @@ function getImports() {
     imports.wbg.__wbindgen_throw = function(arg0, arg1) {
         throw new Error(getStringFromWasm0(arg0, arg1));
     };
-    imports.wbg.__wbindgen_closure_wrapper566 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper567 = function(arg0, arg1, arg2) {
         const ret = makeClosure(arg0, arg1, 174, __wbg_adapter_20);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper920 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper921 = function(arg0, arg1, arg2) {
         const ret = makeMutClosure(arg0, arg1, 227, __wbg_adapter_23);
         return addHeapObject(ret);
     };
