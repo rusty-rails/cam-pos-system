b62cb469ee62321c64f2b3cc9a143e1bfd6d49fb
Christian M <chriamue@gmail.com>
2023-03-26 13:35:04 +0200

    :zap: increase webcam stream performance

--- src/video_stream/webcam_stream.rs
+++ src/video_stream/webcam_stream.rs
@@ -1,6 +1,7 @@
 use image::ImageBuffer;
 use image::Rgb;
 use nokhwa::utils::CameraIndex;
+use nokhwa::Buffer;
 use nokhwa::{
     nokhwa_initialize,
     pixel_format::RgbFormat,
@@ -9,11 +10,14 @@ use nokhwa::{
     CallbackCamera,
 };
 use std::error::Error;
+use std::sync::Arc;
+use std::sync::Mutex;
 
 use super::VideoStream;
 
 pub struct WebcamStream {
     cam: CallbackCamera,
+    current_frame: Arc<Mutex<Option<Buffer>>>,
 }
 
 impl WebcamStream {
@@ -25,9 +29,16 @@ impl WebcamStream {
         cameras.iter().for_each(|cam| println!("{:?}", cam));
         let index = CameraIndex::Index(index as u32);
         let format = RequestedFormat::new::<RgbFormat>(RequestedFormatType::None);
-        let cam = CallbackCamera::new(index, format, |_| {})?;
+        let current_frame = Arc::new(Mutex::new(None));
+        let callback_frame = current_frame.clone();
+        let cam = CallbackCamera::new(index, format, move |buf| {
+            *callback_frame.lock().unwrap() = Some(buf);
+        })?;
 
-        let mut stream = WebcamStream { cam: cam };
+        let mut stream = WebcamStream {
+            cam: cam,
+            current_frame,
+        };
         stream.cam.open_stream()?;
         Ok(stream)
     }
@@ -35,8 +46,13 @@ impl WebcamStream {
 
 impl VideoStream for WebcamStream {
     fn frame(&mut self) -> Result<ImageBuffer<Rgb<u8>, Vec<u8>>, Box<dyn Error>> {
-        let frame = self.cam.poll_frame()?;
-        Ok(frame.decode_image::<RgbFormat>().unwrap())
+        match self.current_frame.lock().unwrap().as_ref() {
+            Some(frame) => Ok(frame.decode_image::<RgbFormat>().unwrap()),
+            None => {
+                let frame = self.cam.poll_frame()?;
+                Ok(frame.decode_image::<RgbFormat>().unwrap())
+            }
+        }
     }
 }
 
